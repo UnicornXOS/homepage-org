69db9e8100af9f5d085834e59441a289e2d4c49f
billlzzz10 <billlzzz10@gmail.com>
2025-10-11 12:42:26 +0700

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** tailwind.config.js */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        blinkblue: '#0D1B2A',
+        blinkviolet: '#6A00FF'
+      }
+    }
+  },
+  plugins: []
+}
